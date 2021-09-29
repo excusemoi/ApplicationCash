4fdbdc15bb2f42c6fbcbb46fb37feaa48aa792b6
excusemoi <abiturient245@gmail.com>
2021-09-29 20:32:49 +0300

    added

---
+++ AppCash.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace ApplicationCashImp
+{
+    public class AppCash<T>
+    {
+        private int _size;
+        private int _currentSize;
+        private Dictionary<string, KeyValuePair<T, DateTime>> _data;
+        private TimeSpan _notesLifeTime;
+        public AppCash(TimeSpan _nlt, int _sz)
+        {
+            _data = new Dictionary<string, KeyValuePair<T, DateTime>>();
+            _notesLifeTime = _nlt;
+            _size = _sz;
+        }
+        public void Save(string key, T data)
+        {
+            if (_data.ContainsKey(key))
+                throw new ArgumentException("The note already exist in the data\n");
+            else if (_currentSize + 1 > _size)
+            {
+                KeyValuePair<string, KeyValuePair<T, DateTime>> tmp = _data.First();
+                foreach (var v in _data)
+                    if (v.Value.Value < tmp.Value.Value)
+                        tmp = v;
+                _data.Remove(tmp.Key);
+            }
+            
+            _data.Add(key, new KeyValuePair<T, DateTime>(data, DateTime.Now));
+            _currentSize++;
+            checkTime(_data);
+        }
+
+        public T Get(string key)
+        {
+            KeyValuePair<T, DateTime> data;
+            if (!_data.TryGetValue(key, out data))
+                throw new KeyNotFoundException($"Key '{key}' data does not exist\n");
+            else if (DateTime.Now - data.Value > _notesLifeTime)
+            {
+                _data.Remove(key);
+                throw new KeyNotFoundException($"Key '{key}' data does not exist\n");
+            }
+            checkTime(_data);
+            return data.Key;
+        }
+        private void checkTime(Dictionary<string, KeyValuePair<T, DateTime>> dict)
+        {
+            foreach (var v in dict)
+            {
+                if (DateTime.Now - v.Value.Value > _notesLifeTime)
+                    dict.Remove(v.Key);
+            }
+        }
+
+        public void printData()
+        {
+            Console.WriteLine("Lifetime of notes");
+            foreach (var v in _data)
+            {
+                Console.WriteLine($"Key {v.Key} Value {v.Value.Key} Added at {v.Value.Value}");
+            }
+        }
+
+        
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using ApplicationCashImp;
+
+
+namespace ApplicationCash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = 0, size = 0, intlifetime = 0;
+            string k = "", data = "";
+            TimeSpan lifetime;
+            try
+            {
+                Console.WriteLine("Enter lifetime of notes (seconds)");
+                int.TryParse(Console.ReadLine(), out intlifetime);    
+                lifetime = TimeSpan.FromSeconds(intlifetime);
+                Console.WriteLine("Enter max size of cash (amount of notes)");
+                int.TryParse(Console.ReadLine(), out size);    
+                AppCash<string> app = new AppCash<string>(lifetime, size);
+                if ((lifetime == TimeSpan.Zero) || (size <= 0))
+                {
+                    Console.WriteLine("Incorrect input");
+                    return;
+                }
+                    printInstruction();
+                while (key != 5)
+                {
+                    int.TryParse(Console.ReadLine(), out key);    
+                    switch (key)
+                    {
+                        case 1:
+                        {
+                            Console.WriteLine("Enter <key>");
+                            k = Console.ReadLine();
+                            Console.WriteLine("Enter <data>");
+                            data = Console.ReadLine();
+                            app.Save(k, data);
+                            break;
+                        }
+                        case 2:
+                        {
+                            Console.WriteLine("Enter <data>");
+                            data = Console.ReadLine();
+                            app.Get(data);
+                            break;
+                        }
+                        case 3:
+                        {
+                            app.printData();
+                            break;
+                        }
+                        case 4:
+                        {
+                            printInstruction();
+                            break;
+                        }
+                        case 5:
+                        {
+                            break;
+                        }
+                        default: break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void printInstruction()
+        {
+            Console.WriteLine("1.Save data by command: <key> <data>");
+            Console.WriteLine("2.Get data by command: <key>");
+            Console.WriteLine("3.View table");
+            Console.WriteLine("4.Repeat instruction");
+            Console.WriteLine("5.Exit");
+        }
+        
+    }
+}
